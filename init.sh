72c1761b154efb8fa63228d3ba663f146f645c56
Christopher Grant <christopher.grant@protonmail.com>
2024-05-08 09:03:26 -0700

    change url

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y # install rust
 
 . "$HOME/.cargo/env" # source the env
 
-cargo install --git https://github.com/christophergrant/deltaInventory
+cargo install --git https://github.com/christophergrant/deltaProfiler
 
 # now the program can be run at /root/.cargo/bin/delta_profiler
